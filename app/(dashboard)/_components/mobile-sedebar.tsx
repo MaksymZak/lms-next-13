84a428426074d4c4af186bdf33ae87c950066778
Maksym-1989 <zaksumy1989@gmail.com>
2023-10-30 19:58:08 +0200

    fix

--- app/(dashboard)/_components/mobile-sedebar.tsx
+++ app/(dashboard)/_components/mobile-sedebar.tsx
@@ -1,16 +1,19 @@
-import { Menu } from 'lucide-react';
+import { Menu } from "lucide-react";
 
-import {Sheet,SheetContent,SheetTrigger} from '@/components/ui/sheet';
-import Sidebar from './sidebar';
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
-    return (
-        <Sheet>
-            <SheetTrigger className='md:hidden pr-4 hover:opacity-75 transition'><Menu /></SheetTrigger>
-            <SheetContent side="left" className='p-0 bg-white' ><Sidebar/></SheetContent>
-        </Sheet>
-    
-    );
-}
+  return (
+    <Sheet>
+      <SheetTrigger className="pr-4 transition hover:opacity-75 md:hidden">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="bg-white p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default MobileSidebar;
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,12 +2,13 @@ import MobileSidebar from "./mobile-sedebar";
 import NavbarRoutes from "@/components/navbar-routes";
 
 const Navbar = () => {
-    return (
-        <div className="p-3 border-b h-full flex items-center bg-white shadow-sm"><MobileSidebar />
-        
-        <NavbarRoutes/>
-        </div>
-    );
-}
+  return (
+    <div className="flex h-full items-center border-b bg-white p-3 shadow-sm">
+      <MobileSidebar />
+
+      <NavbarRoutes />
+    </div>
+  );
+};
 
 export default Navbar;
