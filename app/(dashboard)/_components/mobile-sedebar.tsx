d36c5ca77da6bf171cb2431c5ce368bdc623c20e
Maksym-1989 <zaksumy1989@gmail.com>
2024-05-23 12:35:31 +0300

    fix close sidebar

--- app/(dashboard)/_components/mobile-sedebar.tsx
+++ app/(dashboard)/_components/mobile-sedebar.tsx
@@ -1,15 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import { Menu } from "lucide-react";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
+  const path = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [path]);
+
   return (
-    <Sheet>
-      <SheetTrigger className="pr-4 transition hover:opacity-75 md:hidden">
+    <Sheet open={isOpen}>
+      <SheetTrigger
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="pr-4 transition hover:opacity-75 md:hidden"
+      >
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className="bg-white p-0">
+      <SheetContent
+        onEscapeKeyDown={() => setIsOpen(false)}
+        side="left"
+        onClose={() => setIsOpen(false)}
+        className="bg-white p-0"
+      >
         <Sidebar />
       </SheetContent>
     </Sheet>
